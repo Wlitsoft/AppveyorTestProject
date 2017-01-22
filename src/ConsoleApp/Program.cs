010b356969d6d614451ef0f01dd4eddc6c17033a
李亮 <admin@wlitsoft.com>
2017-01-22 15:22:13 +0800

    初始化提交。

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel()
+            {
+                Id = 1,
+                Name = "tom",
+                Age = 22
+            };
+
+            string result = JsonConvert.SerializeObject(p);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
